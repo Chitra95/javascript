17a3412ab5701ff93e9b7425e8b54624119e3bb2
Chitra95 <149993553+Chitra95@users.noreply.github.com>
2024-01-30 15:46:03 +0000

    comparison and datatypes

---
+++ 04_comparison.js
@@ -0,0 +1,28 @@
+/**************comparisons************* */
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+/*********never use this *************** */
+//console.log("for null")
+// console.log(null > 0); //false
+// console.log(null < 0);  //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+
+// console.log("for undefined")
+// console.log(undefined > 0); //false
+// console.log(undefined < 0);//false
+// console.log(undefined == 0);//false
+// console.log(undefined >=0);//false
+
+
+/******************strict check === ************** */
+console.log("2"==2); //true
+console.log("2"===2); //false
---
+++ datatypes-summary.js
@@ -0,0 +1,51 @@
+/************2 types of data ******* */
+
+//1.Primitive => Call by value
+/*7types 
+ String,Number,Boolean,Null,undefined,Symbol,BigInt */
+//  const score=100;
+//  const scoreValue=10.3
+//  const isLoggedIn=false
+//  const outsideTemp=null
+//  let userEmail; //undefined
+//  let userEmail1=undefined
+
+//  const id=Symbol('123')
+//  const id2=Symbol('123')
+//  console.log(id===id2);         => //false
+ 
+// const bigNumber=235652347634534857345n
+
+
+
+//2.Non Primitive => call by reference
+//Arrays,Objects,Functions
+//array
+const heros=['shaktiman','doga','hanuman'];
+
+// objects
+let myObj={
+    name:'chitra',
+    age:21,
+}
+
+//function
+const myFunction=function(){
+    console.log("hello world");
+}
+
+
+// console.log(typeof score);  // number
+// console.log(typeof scoreValue); //number
+// console.log(typeof isLoggedIn); //boolean
+// console.log(typeof outsideTemp); //object
+// console.log(typeof userEmail1); //undefined
+// console.log(typeof id); //symbol
+// console.log(typeof bigNumber); //bigint
+// console.log(typeof heros); //object
+// console.log(typeof myObj); //object
+// console.log(typeof myFunction); //function
+
+
+
+
