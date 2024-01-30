f06d69117bbc7d2ce75029cad6c372b807bb5649
Chitra95 <149993553+Chitra95@users.noreply.github.com>
2024-01-30 09:11:38 +0000

    conversion of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,41 @@
+/* Conversion to number */
+let score=true;
+//console.log(typeof score);
+//console.log(score);
+
+let valueInNumber=Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//values of datatypes aftr converting them to number
+//"33"  => 33
+//"33abc" => NaN
+//null => 0
+//undefined => NaN
+//true => 1; false=>0
+
+
+
+
+
+/* Conversion to boolean */
+let isLoggedIn=1;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//1=>true; 0=>false
+//"" => false
+//"chitra"=> true
+
+
+
+
+
+/* Conversion to string */
+let someNumber=33;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//33=> 33
