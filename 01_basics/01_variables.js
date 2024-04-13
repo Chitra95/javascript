74328c596095a08a4db37d384c257889416ad9b2
Chitra95 <149993553+Chitra95@users.noreply.github.com>
2024-04-13 08:23:46 +0000

    new

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,7 @@ const accountId=1443; // value cant be changed
 let accountEmail="chit@google.com"; // better practice for scope declaration
 var accountPassword="12345"; /*dont use bcz of issue in block scope and functional scope*/
 accountCity="Bhiwandi"; //never use this way
-let accountState; // you can declare and leave no problem
+let accountState; // you can declare and leave no problem  -> value will be undefined
 
 // accountId=123;  // not allowed
 console.log(accountId);
