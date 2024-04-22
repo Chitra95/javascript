a2f10c4a5ec2de6df07d75cd83f3b6c87301ed37
Chitra95 <149993553+Chitra95@users.noreply.github.com>
2024-04-22 07:10:59 +0000

    new

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,13 @@
+//fuction definition
+
+function sayMyName(){
+    console.log("C");
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("R");
+    console.log("A");
+}
+
+//function call
+sayMyName();
