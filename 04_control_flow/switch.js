69f197740d856f5031fc5a7c240bdc2e7b4bafe4
Chitra95 <149993553+Chitra95@users.noreply.github.com>
2024-04-25 17:58:30 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,33 @@
+//used in redux
+
+//SYNTAX
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+//EXAMPLE
+const month=2
+
+// switch (month) {
+//     case 1:
+//         console.log("JANUARY");
+//         break;
+//     case 2:
+//         console.log("FEBRUARY");   //since 2    FEBRUARY
+//         break;
+//     case 3:
+//         console.log("MARCH");
+//         break;
+        
+//         case 4:
+//             console.log("APRIL ");
+//             break;
+//     default:
+//         console.log("no month");
+//         break;
+// }
