953ada91e8d0df619b3c3e3ed1c886df1a1f756f
Chitra95 <149993553+Chitra95@users.noreply.github.com>
2024-05-06 19:09:37 +0000

    done p6

---
+++ proj6/script.js
@@ -0,0 +1,35 @@
+// console.log('Project 5');
+
+const insert=document.querySelector("#insert")
+
+window.addEventListener("keydown",(e)=>{
+
+  insert.innerHTML=`
+<div id="color">
+
+<table>
+  <tr>
+    <th>KEY</th>
+    <th>KEYCODE</th>
+    <th>CODE</th>
+  </tr>
+  <tr>
+    <td>${e.key ===' '? "Space" : e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+
+</table>
+
+
+
+</div>
+`
+
+
+})
+
+
+
+
+
