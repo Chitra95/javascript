38af15984f61011274eae714c3a6c51ce9424403
Chitra95 <149993553+Chitra95@users.noreply.github.com>
2024-05-23 04:29:16 +0000

    oop

--- 10_classes_and_oop/oop.js
+++ 10_classes_and_oop/oop.js
@@ -1,26 +1,26 @@
 //Object Literal
- const user={
-    username:"hitesh",
-    loginCount:8,
-    signedIn:true,
+//  const user={
+//     username:"hitesh",
+//     loginCount:8,
+//     signedIn:true,
 
-    getUserDetails:function(){
-        console.log("Got user details from database");
-        console.log(`Username: ${this.username}`);
-        console.log(this);
-    }
- }
+//     getUserDetails:function(){
+//         console.log("Got user details from database");
+//         console.log(`Username: ${this.username}`);
+//         console.log(this);
+//     }
+//  }
 
- console.log(user.username);
- console.log(user["username"]);
+//  console.log(user.username);
+//  console.log(user["username"]);
 
- console.log(user.getUserDetails());
+//  console.log(user.getUserDetails());
 
  /*
  hitesh
 hitesh
 Got user details from database
-Username; hitesh
+Username: hitesh
 {
   username: 'hitesh',
   loginCount: 8,
@@ -29,3 +29,43 @@ Username; hitesh
 }
 undefined
  */
+
+
+//---------------Chontructor function
+// const promiseOne=new Promise()
+const date=new Date()
+
+function User(username,email,password){
+  this.username=username;
+  this.email=email;
+  this.password=password;
+
+  this.greeting=function(){
+    console.log(`Welcome ${this.username}`);
+  }
+
+  return this
+}
+
+const userOne= new User("hitesh","h@gmail.com","123")
+console.log(userOne)
+console.log(userOne.greeting())
+console.log(userOne.constructor)
+
+/*User {
+  username: 'hitesh',
+  email: 'h@gmail.com',
+  password: '123',
+  greeting: [Function (anonymous)]
+}
+Welcome hitesh
+undefined
+[Function: User]*/
+
+
+// const userTwo=new User("h","h@gmail.com","123")
+// console.log(userTwo)
+
+/* User { username: 'hitesh', email: 'h@gmail.com', password: '123' }
+User { username: 'h', email: 'h@gmail.com', password: '123' } */
+
